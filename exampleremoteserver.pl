3faa613ec78b1d752f058ade8e3b7d01e5d6a7e4
mangaroo@gmail.com <mangaroo@gmail.com@9b2bcba1-3ade-9cf8-2c58-42301fe886be>
2011-05-22 06:26:34 +0000

    Fixed problems with local execution of code, minor code changes. Still have issues with execution of code over XML-RPC.

--- exampleremoteserver.pl
+++ exampleremoteserver.pl
@@ -1,26 +1,5 @@
 #!/usr/bin/env perl
 
-################################################################################
-# This library is free software; you can redistribute it and/or modify it under 
-# the terms of the GNU Lesser General Public License as published by the Free 
-# Software Foundation; either version 2.1 of the License, or (at your option) 
-# any later version.
-
-# This library is distributed in the hope that it will be useful, but WITHOUT 
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
-# details.
-
-# You should have received a copy of the GNU Lesser General Public License along 
-# with this library; if not, write to the Free Software Foundation, Inc., 
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-################################################################################
-
-# Author: David Luu
-
-# This is a demo script showing runtime loading of an external test library into
-# remote server for execution to be called by Robot Framework over XML-RPC.
-
 #use strict;
 #use warnings;
 
@@ -36,7 +15,8 @@ use ExampleLibrary;
 #or alternatively implement the test library in this file
 
 #create instance of remote server with specified library
-my $remote_svr = new RobotRemoteServer("ExampleLibrary");
+#my $remote_svr = new RobotRemoteServer("ExampleLibrary");
+
 #alternatively, specified like...
 #my $remote_svr = new RobotRemoteServer("ExampleRemoteLibrary.pm");
 #my $remote_svr = new RobotRemoteServer("ExampleRemoteLibrary.pl");
@@ -44,46 +24,54 @@ my $remote_svr = new RobotRemoteServer("ExampleLibrary");
 #set address & port like this
 #my $remote_svr = new RobotRemoteServer("ExampleRemoteLibrary","my.domain.com",8080);
 
-#DEBUG blocks showing local execution rather than via XML-RPC
-print "Debugging info...\n\n";
+#disable stopping of remote server remotely like this:
+my $remote_svr = new RobotRemoteServer("ExampleRemoteLibrary","localhost",8080,0);
+#by default, stopping server remotely is enabled (e.g. value of 1)
+
+#DEBUG showing details of local execution
+print "Debugging info: showing details of local execution...\n\n";
 print "keywords:\n";
-my @res = $remote_svr->get_keyword_names();
-foreach (@res){
-	print $_."\n";
+my @kws = $remote_svr->get_keyword_names();
+foreach(@kws){
+  print $_."\n";
 }
 print "\n";
 
-print "runkeyword:\n";
+print "running keywords:\n";
 print "\n";
 
-print "string equal:\n";
-print "\n";
 my @stat = $remote_svr->run_keyword("strings_should_be_equal","hello","world");
-foreach (@stat){
-	print $_."\n";
+foreach(@stat){
+  while ( my ($key, $value) = each(%$_) ) {
+        print "$key => $value\n";
+    }
 }
 print "\n";
 
-print "count dir:\n";
-print "\n";
 my @stat = $remote_svr->run_keyword("count_items_in_directory","C:\\Temp");
-foreach (@stat){
-	print $_."\n";
+foreach(@stat){
+  while ( my ($key, $value) = each(%$_) ) {
+        print "$key => $value\n";
+    }
 }
 print "\n";
 
 #This has been tested to work offline. Comment out so can test server online, 
 #else this will shut down the server already before it starts.
-#my @stat = $remote_svr->run_keyword("stop_remote_server");
-#foreach (@stat){
-#	print $_."\n";
-#}
+my @stat = $remote_svr->run_keyword("stop_remote_server");
+foreach(@stat){
+  while ( my ($key, $value) = each(%$_) ) {
+        print "$key => $value\n";
+    }
+}
 print "\n";
 
 #start remote server
-print "Robot remote server started. Stop server with Ctrl+C, kill, etc. or XML-RPC method 'run_keyword' with parameter 'stop_remote_server'\n";
+print "Robot remote server started. Stop server with Ctrl+C, kill, etc. or XML-RPC method 'run_keyword' with parameter 'stop_remote_server'\n\n";
+
+print "Debugging info: here is where we have XML-RPC problems with Perl reflection once try use XML-RPC. As can see from runtime demo above, no issues using reflection locally.\n\n";
+
 $remote_svr->start_server();
-#now can test with XML-RPC requests to server
 
 #ending script/module return value to append below
 1;
